2c89e4494138634faecab2b5aa52c31d3f6b6d17
user <fjodor.la@gmail.com>
2020-09-14 19:09:20 +0300

    20200914_19_10

---
+++ JAVA/MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello EDIBO");
+        System.out.println("Hello EDIBO");
+        String name = "Jameees";
+        System.out.println(name);
+        final int myNum = 25;
+        int myNum1 = 15;
+        // myNum = 25;
+        System.out.println(myNum);
+        System.out.println("Hello " + name + " , I know that your age is " + myNum + " however you act like you are " + myNum1 );
+
+    }
+}
